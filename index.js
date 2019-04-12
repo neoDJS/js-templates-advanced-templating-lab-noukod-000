e9ee569b9ae43de4d0d43ff0e6e898da0fd72856
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-12 15:22:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function init() {
   handlebarsRegisters();
 
   let recipeFormTemplateFn = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
-  document.getElementById('main').innerHTML = recipeFormTemplateFn( {formLegend: 'New Recipe', submitFunction: 'createRecipe()', state: "New"} );
+  document.getElementById('main').innerHTML = recipeFormTemplateFn( {formLegend: 'New Recipe', handleSubmit: 'createRecipe()', state: "New"} );
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
@@ -22,8 +22,6 @@ function handlebarsRegisters(){
   });
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
 }
-
-function handleSubmit(){}
 
 function getRecipe(){
   let recipe = {ingredients:[]};
