fff949f71c8d83d85a9f2fb85bd0c988b9d5e527
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-12 14:35:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial',document.getElementById('recipe-details-partial').innerHTML);
   Handlebars.registerHelper('displayIngredient',function(){
-    return new Handlebars.SafeString(
-      "<li name='ingredientsList'>"+this.name+"</li>"
-    );
+    return new Handlebars.SafeString(this.name);
   });
 
   let recipeFormTemplateFn = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
